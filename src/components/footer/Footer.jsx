6117824516fc6b7c66a51ac3eb4ce058fb73ffef
marianamarinflor622 <marianamarinflor@gmail.com>
2025-01-28 15:33:00 +0100

    footer-done

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,31 @@
+import './Footer.css';
+import twitter from '../../assets/twitter.svg'
+import facebbok from '../../assets/facebook.svg'
+
+function Footer() {
+  return (
+    
+    <div>
+
+    <footer className="footer">
+
+
+      <p>About us</p>  
+      
+      <p>Contact</p>
+      <div className="twitter">
+      <img src={twitter} alt="Logo twiter"/>
+       <p>Twitter</p>
+       </div>
+       <div className="facebook">
+         <img src={facebbok} alt="Logo facebook"/>
+         <p>Facebook</p>
+       </div>
+     
+    </footer>
+    </div>
+
+  );
+}       
+
+export default Footer;   
