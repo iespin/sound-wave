3baaa14d39782d7f11b2120f0e219181a2953966
Priscila Oliveira <pguillen.oliv@gmail.com>
2025-01-30 10:42:35 +0100

    fixes css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,22 +6,27 @@ function PageOne () {
 return (
 
     <div className={styles.homeCard}>
-        <img className={styles.girlImg} src={landingGirl} alt="image of a girl wit☺h headphones on"/>
-        <div className={styles.circleTop}></div>
 
-        <div className={styles.textButtonCard}>
-
-                <h1 className={styles.titlePageOne}>Feel <br></br>the Music</h1>
-
-                <p className={styles.textPageOne}>Stream over 20 thousand songs with one click</p>
-        
-        </div>
-
-        <div className={styles.circleBottomLeft}></div>
-
-        <div className={styles.circleBottomRight}></div>
-
+    <div className={styles.imageContainer}>
+        <img className={styles.girlImg} src={landingGirl} alt="image of a girl with headphones on"/>
     </div>
+        
+    <div className={styles.circleTop}></div>
+
+    <div className={styles.textButtonCard}>
+
+        <h1 className={styles.titlePageOne}>Feel the Music</h1>
+
+        <p className={styles.textPageOne}>Stream over 20 thousand songs with one click</p>
+        
+    </div>
+
+    <div className={styles.circleBottomLeft}></div>
+
+    <div className={styles.circleBottomRight}></div>
+
+</div>
+
 )
 
 }
