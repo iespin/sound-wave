6d5eb8f6ef95428f25f47c21b7cd33f33a5fda46
Israel Espin <israel.espin92@gmail.com>
2025-02-03 10:04:20 +0100

    deleted comments

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -11,10 +11,7 @@ describe('home page navigation', () => {
 })
 
 describe('home page content', () => {
-  // it.only('should render the navbar', () => {
-  //   cy.visit('/')
-  //   cy.get('HeaderNav').should('exist')
-  // })
+  
   it('the h1 contains the correct text', () => {
     cy.visit('/')
     cy.get('h1').contains('Feel the Music')
@@ -23,8 +20,5 @@ describe('home page content', () => {
     cy.visit('/')
     cy.get('p').contains('Stream over 20 thousand songs with one click')
   })
-  // it('should render the Join Now Button', () => {
-  //   cy.visit('/')
-  //   cy.get('JoinNow').should('exist')
-  // })
+ 
 })
